c7fabfb6ef0b2234f9c886e2c0982c40cbaf174c
BlackFire-Desktop <GMate375@gmail.com>
2018-09-30 19:46:41 +0200

    Added Spotify WebAPI .NET wrapper library and empty services.

---
+++ TrackTracker/Services/SpotifyService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+
+using TrackTracker.Services.Interfaces;
+
+namespace TrackTracker.Services
+{
+    public class SpotifyService : ISpotifyService
+    {
+
+    }
+}
