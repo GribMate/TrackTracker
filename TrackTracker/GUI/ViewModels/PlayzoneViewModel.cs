fcf9200749e02180ab763d5be85525cfecaf25bc
BlackFire-Desktop <GMate375@gmail.com>
2018-11-07 00:35:01 +0100

    REFACTOR: Added all VMs.

---
+++ TrackTracker/GUI/ViewModels/PlayzoneViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+using WinForms = System.Windows.Forms;
+
+using TrackTracker.Services.Interfaces;
+using TrackTracker.BLL;
+using TrackTracker.BLL.Enums;
+
+
+
+namespace TrackTracker.GUI.ViewModels
+{
+    public class PlayzoneViewModel : ViewModelBase
+    {
+        public PlayzoneViewModel() : base()
+        {
+
+        }
+    }
+}
