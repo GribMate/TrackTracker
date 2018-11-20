e2d5152ba16e2272538fde31a294b4422f621479
BlackFire-Desktop <GMate375@gmail.com>
2018-11-20 01:24:42 +0100

    REFACTOR: CommonControls MVVM.

--- TrackTracker/GUI/ViewModels/CommonControlsViewModel.cs
+++ TrackTracker/GUI/ViewModels/CommonControlsViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
 using System.Windows.Input;
 
 using WinForms = System.Windows.Forms;
@@ -16,9 +19,139 @@ namespace TrackTracker.GUI.ViewModels
 {
     public class CommonControlsViewModel : ViewModelBase
     {
+        private IEnvironmentService environmentService;
+
+        private Timer onlineStatusTimer;
+
         public CommonControlsViewModel() : base()
         {
+            environmentService = DependencyInjector.GetService<IEnvironmentService>();
 
+            OnlineStatus = "Checking...";
+            OnlineStatusForeground = Brushes.Yellow;
+            ProcessingNameVisibility = Visibility.Hidden;
+            ProcessingValueVisibility = Visibility.Hidden;
+            ProgressBarVisibility = Visibility.Hidden;
+
+            onlineStatusTimer = new Timer(SetOnlinestatusUIElement, null, 3000, 5000); // Checking in every 5 sec, waiting 3 sec before first check to ensure that GUI is loaded
+        }
+
+
+
+        private string onlineStatus;
+        public string OnlineStatus
+        {
+            get => onlineStatus;
+            set
+            {
+                SetProperty(ref onlineStatus, value);
+            }
+        }
+
+        private string processingName;
+        public string ProcessingName
+        {
+            get => processingName;
+            set
+            {
+                SetProperty(ref processingName, value);
+            }
+        }
+
+        private string processingValue;
+        public string ProcessingValue
+        {
+            get => processingValue;
+            set
+            {
+                SetProperty(ref processingValue, value);
+            }
+        }
+
+        private int progressBarValue;
+        public int ProgressBarValue
+        {
+            get => progressBarValue;
+            set
+            {
+                SetProperty(ref progressBarValue, value);
+            }
+        }
+
+        private Visibility processingNameVisibility;
+        public Visibility ProcessingNameVisibility
+        {
+            get => processingNameVisibility;
+            set
+            {
+                SetProperty(ref processingNameVisibility, value);
+            }
+        }
+
+        private Visibility processingValueVisibility;
+        public Visibility ProcessingValueVisibility
+        {
+            get => processingValueVisibility;
+            set
+            {
+                SetProperty(ref processingValueVisibility, value);
+            }
+        }
+
+        private Visibility progressBarVisibility;
+        public Visibility ProgressBarVisibility
+        {
+            get => progressBarVisibility;
+            set
+            {
+                SetProperty(ref progressBarVisibility, value);
+            }
+        }
+
+        private SolidColorBrush onlineStatusForeground;
+        public SolidColorBrush OnlineStatusForeground
+        {
+            get => onlineStatusForeground;
+            set
+            {
+                SetProperty(ref onlineStatusForeground, value);
+            }
+        }
+
+
+
+        private void SetOnlinestatusUIElement(object state)
+        {
+            if (environmentService.InternetConnectionIsAlive()) // We have connection
+            {
+                OnlineStatus = "Connected";
+                OnlineStatusForeground = Brushes.LawnGreen;
+            }
+            else // None or failed internet connection
+            {
+                OnlineStatus = "Disconnected";
+                OnlineStatusForeground = Brushes.Red;
+            }
+        }
+
+
+
+        public void SetProgressBarValue(int value, string description)
+        {
+            if (value > 0)
+            {
+                ProcessingNameVisibility = Visibility.Visible;
+                ProcessingValueVisibility = Visibility.Visible;
+                ProgressBarVisibility = Visibility.Visible;
+            }
+            else if (value == 0)
+            {
+                ProcessingNameVisibility = Visibility.Hidden;
+                ProcessingValueVisibility = Visibility.Hidden;
+                ProgressBarVisibility = Visibility.Hidden;
+            }
+            ProgressBarValue = value;
+            ProcessingValue = description;
         }
     }
 }
--- TrackTracker/GUI/Views/CommonControls.xaml.cs
+++ TrackTracker/GUI/Views/CommonControls.xaml.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Threading;
-using System.Windows;
-using System.Windows.Threading;
 using System.Windows.Controls;
-using System.Windows.Media;
-
-using TrackTracker.BLL;
-using TrackTracker.Services.Interfaces;
 
 
 
@@ -14,50 +7,9 @@ namespace TrackTracker.GUI.Views
 {
     public partial class CommonControls : UserControl
     {
-        private Timer onlineStatusTimer;
-
         public CommonControls()
         {
             InitializeComponent();
-
-            onlineStatusTimer = new Timer(SetOnlinestatusUIElement, null, 3000, 5000);
-            //checking in every 5 sec, waiting 3 sec before first check to ensure that GUI is loaded
-        }
-
-        private void SetOnlinestatusUIElement(object state)
-        {
-            labelOnlineStatus.Dispatcher.Invoke(() =>
-            {
-                if (DependencyInjector.GetService<IEnvironmentService>().InternetConnectionIsAlive()) //we have connection
-                {
-                    labelOnlineStatus.Content = "Connected";
-                    labelOnlineStatus.Foreground = Brushes.LawnGreen;
-                }
-                else //no or failed internet connection
-                {
-                    labelOnlineStatus.Content = "Disconnected";
-                    labelOnlineStatus.Foreground = Brushes.Red;
-                }
-            }
-            );
-        }
-
-        public void SetProgressBarValue(int value, string description)
-        {
-            if (value > 0)
-            {
-                labelProcessingName.Visibility = Visibility.Visible;
-                labelProcessingValue.Visibility = Visibility.Visible;
-                progressBar.Visibility = Visibility.Visible;
-            }
-            else if (value == 0)
-            {
-                labelProcessingName.Visibility = Visibility.Hidden;
-                labelProcessingValue.Visibility = Visibility.Hidden;
-                progressBar.Visibility = Visibility.Hidden;
-            }
-            progressBar.Dispatcher.Invoke(() => progressBar.Value = value, DispatcherPriority.Background);
-            labelProcessingValue.Content = description;
         }
     }
 }
